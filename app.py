6c74559449b535f8200689e5cdd234b90b2cb170
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-15 18:11:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -113,8 +113,6 @@ uploaded_file = st.file_uploader("Carica il tuo file Excel", type=['xlsx'])
 if uploaded_file is not None:
     df = load_data(uploaded_file)
     if not df.empty:
-        df_final = pivot_sizes(df)
-        df_final = convert_excel_dates(df_final)  # Converte le date nel formato corretto
         processed_data, processed_file_name = convert_df_to_excel(df_final, uploaded_file.name)
         st.download_button(
             label="📥 Scarica Excel",
