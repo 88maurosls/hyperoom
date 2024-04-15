37348712d8e9bc71279b5484f24f0128ed01852d
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-15 12:55:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,18 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
+def clean_sizes_column(df, column_name='Sizes'):
+    # Assicurati che la colonna esista nel DataFrame
+    if column_name in df.columns:
+        # Rimuovi la parola "Sizes" dalla fine dei valori della colonna
+        df[column_name] = df[column_name].str.replace('Sizes', '').str.strip()
+    return df
+
 def convert_df_to_excel(df):
     output = BytesIO()
-    # Utilizzo il contesto di 'with' per assicurarmi che tutte le operazioni sul file siano chiuse correttamente
     with pd.ExcelWriter(output, engine='openpyxl') as writer:
         df.to_excel(writer, sheet_name='Sheet1', index=False)
-        # Non è necessario chiamare writer.save() qui, perché il contesto 'with' gestisce la chiusura
-    output.seek(0)  # Riportiamo il cursore del file al suo inizio dopo aver finito di scrivere
+    output.seek(0)
     return output
 
 st.title('Applicazione per l\'estrazione di dati Excel')
@@ -17,7 +22,8 @@ uploaded_file = st.file_uploader("Carica il tuo file Excel", type=['xlsx'])
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
     if not df.empty:
-        st.write("Anteprima dei dati caricati:", df)
+        df = clean_sizes_column(df)  # Pulisci la colonna Sizes
+        st.write("Anteprima dei dati corretti:", df)
         processed_data = convert_df_to_excel(df)  # Converti il DataFrame in Excel
         
         st.download_button(
