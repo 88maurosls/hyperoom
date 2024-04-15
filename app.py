00ce414b6960572afdf4a2269b6adb0c2b6e4146
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-15 12:49:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
+import streamlit as st
+import pandas as pd
+import os
+from io import BytesIO
+
+def convert_df_to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='openpyxl')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+st.title('Applicazione per l\'estrazione di dati Excel')
+
+uploaded_file = st.file_uploader("Carica il tuo file Excel", type=['xlsx'])
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    st.write("Anteprima dei dati caricati:", df)
+    processed_data = convert_df_to_excel(df)  # Converti il DataFrame in Excel
+
+    st.download_button(
+        label="📥 Scarica dati Excel",
+        data=processed_data,
+        file_name='dati_processati.xlsx',
+        mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
 
