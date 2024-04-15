7c029640264f7ccb2634845f31a3a2db7e71f0ac
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-15 17:56:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def convert_excel_dates(df):
         df[col] = pd.to_datetime(df[col], unit='d').dt.strftime('%Y-%m-%d')
     return df
 
-st.title('Applicazione per la trasposizione e raggruppamento dei dati Excel')
+st.title('Hyperoom to Excel v2.1 BETA')
 
 uploaded_file = st.file_uploader("Carica il tuo file Excel", type=['xlsx'])
 if uploaded_file is not None:
